27892fd2691e4025aa142dbd6c97b7455933a05c
gimunkwon <gimon71@gmail.com>
2026-01-25 13:08:48 +0900

    Update: BaseCharacter Logic With GAS System

---
+++ Source/LostArk_SkillGAS/Private/Character/Base/LA_BaseCharacter.cpp
@@ -0,0 +1,48 @@
+﻿#include "Character/Base/LA_BaseCharacter.h"
+#include "AbilitySystemComponent.h"
+
+
+ALA_BaseCharacter::ALA_BaseCharacter()
+{
+	PrimaryActorTick.bCanEverTick = false;
+	
+	// ASC 생성
+	AbilitySystemComponent = CreateDefaultSubobject<UAbilitySystemComponent>(TEXT("AbilitySystemComponent"));
+	AbilitySystemComponent->SetIsReplicated(true);
+	AbilitySystemComponent->SetReplicationMode(EGameplayEffectReplicationMode::Mixed);
+}
+
+
+void ALA_BaseCharacter::BeginPlay()
+{
+	Super::BeginPlay();
+	
+	InitAbilityActorInfo();
+}
+
+UAbilitySystemComponent* ALA_BaseCharacter::GetAbilitySystemComponent() const
+{
+	return AbilitySystemComponent;
+}
+
+void ALA_BaseCharacter::InitAbilityActorInfo()
+{
+	// 오버라이드 예정
+}
+
+void ALA_BaseCharacter::InitializeAttributes()
+{
+	// 데이터 에셋으로부터 값을 읽어와서 GamePlayEffect로 스탯을 꽂아넣는 로직
+}
+
+void ALA_BaseCharacter::AddCharacterAbilities()
+{
+	// 기본 공격, 스킬 등을 ASC에 등록하는 로직
+}
+
+void ALA_BaseCharacter::Die()
+{
+	// 사망 애니메이션 재생, 콜라이더 끄기
+}
+
+
---
+++ Source/LostArk_SkillGAS/Public/Character/Base/LA_BaseCharacter.h
@@ -0,0 +1,45 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/Character.h"
+#include "AbilitySystemInterface.h"
+#include "LA_BaseCharacter.generated.h"
+
+class UWidgetComponent;
+class UAttributeSet;
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ALA_BaseCharacter : public ACharacter, public IAbilitySystemInterface
+{
+	GENERATED_BODY()
+public:
+	ALA_BaseCharacter();
+	
+	virtual UAbilitySystemComponent* GetAbilitySystemComponent() const override;
+protected:
+	virtual void BeginPlay() override;
+#pragma region ASC
+	// GAS 핵심 컴포넌트
+	UPROPERTY(VisibleAnywhere,Category="GAS")
+	TObjectPtr<UAbilitySystemComponent> AbilitySystemComponent;
+	UAttributeSet* GetAttributeSet() const {return AttributeSet;}
+	UPROPERTY()
+	TObjectPtr<UAttributeSet> AttributeSet;
+#pragma endregion
+	
+	// 공통 외형 및 정보
+	UPROPERTY(EditAnywhere,Category="CharacterInfo | Name")
+	FText CharacterName;
+	UPROPERTY(VisibleAnywhere, BlueprintReadOnly, Category="UI")
+	TObjectPtr<UWidgetComponent> CommonWidgetComponent;
+	UPROPERTY(EditAnywhere,BlueprintReadOnly,Category="UI")
+	TSubclassOf<UUserWidget> MainHUDWidgetClass;
+	
+	// 초기화 로직
+	virtual void InitAbilityActorInfo(); // ASC와 액터 연결
+	virtual void InitializeAttributes(); // 기본 스탯 주입
+	virtual void AddCharacterAbilities(); // 기본 스킬 부여
+	
+	// 사망 로직
+	virtual void Die();
+};
