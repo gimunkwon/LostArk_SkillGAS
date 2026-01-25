fc052fa58d930d6a9ec341c977e08748f914ca36
gimunkwon <gimon71@gmail.com>
2026-01-25 13:08:20 +0900

    Update : ClassAttributeSet

---
+++ Source/LostArk_SkillGAS/Private/Character/Player/Attribute/LA_ClassAttributeset.cpp
@@ -0,0 +1,4 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Character/Player/Attribute/LA_ClassAttributeset.h"
---
+++ Source/LostArk_SkillGAS/Public/Character/Player/Attribute/LA_ClassAttributeset.h
@@ -0,0 +1,16 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "AttributeSet.h"
+#include "LA_ClassAttributeset.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class LOSTARK_SKILLGAS_API ULA_ClassAttributeset : public UAttributeSet
+{
+	GENERATED_BODY()
+};
