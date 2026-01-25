a8e1d6100cbd3c70620aa1de9698512145c07eaa
gimunkwon <gimon71@gmail.com>
2026-01-25 13:08:06 +0900

    Add:Global Macro

--- Source/LostArk_SkillGAS/LostArk_SkillGAS.h
+++ Source/LostArk_SkillGAS/LostArk_SkillGAS.h
@@ -1,6 +1,10 @@
-// Copyright Epic Games, Inc. All Rights Reserved.
-
 #pragma once
 
 #include "CoreMinimal.h"
 
+// GAS 표준 매크로
+#define ATTRIBUTE_ACCESSORS(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_PROPERTY_GETTER(ClassName, PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_GETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_SETTER(PropertyName) \
+GAMEPLAYATTRIBUTE_VALUE_INITTER(PropertyName)
