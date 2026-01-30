4bc45c9b7a353110f1f6b15566007f12e08a74f7
gimunkwon <gimon71@gmail.com>
2026-01-30 20:33:58 +0900

    Update: PlayerWidget HP,MP ProgressBar

---
+++ Source/LostArk_SkillGAS/Private/UI/Widget/Player/LA_PlayerHUDWidget.cpp
@@ -0,0 +1,19 @@
+﻿#include "UI/Widget/Player/LA_PlayerHUDWidget.h"
+
+#include "Components/ProgressBar.h"
+
+void ULA_PlayerHUDWidget::UpdateHealth(float CurrentHealth, float MaxHealth)
+{
+	if (HPBar && MaxHealth > 0.f)
+	{
+		HPBar->SetPercent(CurrentHealth / MaxHealth);
+	}
+}
+
+void ULA_PlayerHUDWidget::UpdateMana(float CurrentMana, float MaxMana)
+{
+	if (MPBar && MaxMana > 0.f)
+	{
+		MPBar->SetPercent(CurrentMana / MaxMana);
+	}
+}
---
+++ Source/LostArk_SkillGAS/Public/UI/Widget/Player/LA_PlayerHUDWidget.h
@@ -0,0 +1,23 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LA_PlayerHUDWidget.generated.h"
+
+class UProgressBar;
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ULA_PlayerHUDWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(meta=(BindWidget))
+	UProgressBar* HPBar;
+	UPROPERTY(meta=(BindWidget))
+	UProgressBar* MPBar;
+	
+	// 데이터를 업데이트하는 함수
+	void UpdateHealth(float CurrentHealth, float MaxHealth);
+	void UpdateMana(float CurrentMana, float MaxMana);
+};
