c4b886ad78905425de808dfffb172738b9145311
gimunkwon <gimon71@gmail.com>
2026-01-30 21:40:35 +0900

    Update:Initialize HUD Logic

---
+++ Source/LostArk_SkillGAS/Private/UI/HUD/LA_HUD.cpp
@@ -0,0 +1,15 @@
+﻿#include "UI/HUD/LA_HUD.h"
+
+#include "Blueprint/UserWidget.h"
+#include "UI/Widget/Player/LA_PlayerHUDWidget.h"
+
+
+void ALA_HUD::InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS)
+{
+	if (PlayerHUDWidgetClass)
+	{
+		PlayerHUDWidget = CreateWidget<ULA_PlayerHUDWidget>(PC, PlayerHUDWidgetClass);
+		PlayerHUDWidget->AddToViewport();
+		// 초기값 설정및 델리게이트 바인딩
+	}
+}
---
+++ Source/LostArk_SkillGAS/Public/UI/HUD/LA_HUD.h
@@ -0,0 +1,25 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/HUD.h"
+#include "LA_HUD.generated.h"
+
+class ULA_PlayerHUDWidget;
+class UAttributeSet;
+class UAbilitySystemComponent;
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ALA_HUD : public AHUD
+{
+	GENERATED_BODY()
+	
+public:
+	void InitOverlay(APlayerController* PC, APlayerState* PS, UAbilitySystemComponent* ASC, UAttributeSet* AS);
+	
+	UPROPERTY()
+	TObjectPtr<ULA_PlayerHUDWidget> PlayerHUDWidget;
+protected:
+	UPROPERTY(EditAnywhere)
+	TSubclassOf<UUserWidget> PlayerHUDWidgetClass;
+	
+};
