01429de9e3ec78fbe51f7121b17436cc21c1a414
gimunkwon <gimon71@gmail.com>
2026-01-30 12:11:44 +0900

    Update: GameInstance Settings

---
+++ Source/LostArk_SkillGAS/Private/Global/GameInstance/LA_GameInstance.cpp
@@ -0,0 +1,19 @@
+﻿// Fill out your copyright notice in the Description page of Project Settings.
+
+
+#include "Global/GameInstance/LA_GameInstance.h"
+
+#include "Kismet/KismetSystemLibrary.h"
+
+void ULA_GameInstance::Init()
+{
+	Super::Init();
+	
+	// 모든 창(서버/클라이언트)의 프레임을 60으로 고정
+	UKismetSystemLibrary::ExecuteConsoleCommand(GetWorld(), TEXT("t.MaxFPS 60"));
+	
+	// FPS 표시
+	UKismetSystemLibrary::ExecuteConsoleCommand(GetWorld(), TEXT("Stat FPS"));
+	
+	UE_LOG(LogTemp, Warning, TEXT("GameInstance Initialized: FPS Fixed to 60"));
+}
---
+++ Source/LostArk_SkillGAS/Public/Global/GameInstance/LA_GameInstance.h
@@ -0,0 +1,15 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Engine/GameInstance.h"
+#include "LA_GameInstance.generated.h"
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ULA_GameInstance : public UGameInstance
+{
+	GENERATED_BODY()
+	
+public:
+	// 게임 인스턴스가 초기화 될 때 호출(서버/클라이언트 공통)
+	virtual void Init() override;
+};
