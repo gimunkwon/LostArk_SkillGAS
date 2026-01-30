9d12798301972a76423c04e791537d3695424cca
gimunkwon <gimon71@gmail.com>
2026-01-30 12:11:26 +0900

    Update: Add Player ClickToMove Logic

---
+++ Source/LostArk_SkillGAS/Private/Controller/Player/LA_PlayerController.cpp
@@ -0,0 +1,47 @@
+﻿#include "Controller/Player/LA_PlayerController.h"
+
+#include "EnhancedInputComponent.h"
+#include "EnhancedInputSubsystems.h"
+#include "Character/Player/Class/LA_BasePlayer.h"
+
+ALA_PlayerController::ALA_PlayerController()
+{
+	bShowMouseCursor = true;
+	DefaultMouseCursor = EMouseCursor::Default;
+	bReplicates = true;
+}
+
+void ALA_PlayerController::BeginPlay()
+{
+	Super::BeginPlay();
+	if (UEnhancedInputLocalPlayerSubsystem* SubSystem = ULocalPlayer::GetSubsystem<UEnhancedInputLocalPlayerSubsystem>(GetLocalPlayer()))
+	{
+		SubSystem->AddMappingContext(DefaultMappingContext, 0);
+	}
+}
+
+void ALA_PlayerController::SetupInputComponent()
+{
+	Super::SetupInputComponent();
+	
+	if (UEnhancedInputComponent* EInputComp = CastChecked<UEnhancedInputComponent>(InputComponent))
+	{
+		EInputComp->BindAction(IA_ClickToMove,ETriggerEvent::Started,this,&ALA_PlayerController::OnInputStarted);
+		EInputComp->BindAction(IA_ClickToMove, ETriggerEvent::Triggered, this, &ALA_PlayerController::OnInputStarted);
+	}
+}
+
+void ALA_PlayerController::OnInputStarted()
+{
+	FHitResult Hit;
+	if (GetHitResultUnderCursor(ECC_Visibility,false, Hit))
+	{
+		if (ALA_BasePlayer* MyPawn = Cast<ALA_BasePlayer>(GetPawn()))
+		{
+			// 플레이어 이동 RPC 함수
+			Hit.Location.Z = 0.f;
+			UE_LOG(LogTemp,Warning,TEXT("OnInputStarted"));
+			MyPawn->RequestToMove(Hit.Location);
+		}
+	}
+}
---
+++ Source/LostArk_SkillGAS/Public/Controller/Player/LA_PlayerController.h
@@ -0,0 +1,28 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/PlayerController.h"
+#include "LA_PlayerController.generated.h"
+
+class UInputMappingContext;
+class UInputAction;
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ALA_PlayerController : public APlayerController
+{
+	GENERATED_BODY()
+public:
+	ALA_PlayerController();
+protected:
+	virtual void BeginPlay() override;
+	virtual void SetupInputComponent() override;
+	// Default MappingContext
+	UPROPERTY(EditAnywhere,Category="Input|MappingContext")
+	UInputMappingContext* DefaultMappingContext;
+	// InputAction
+	UPROPERTY(EditAnywhere,Category="Input|InputAction")
+	UInputAction* IA_ClickToMove;
+	// InputAction Binding Function
+	void OnInputStarted();
+	
+};
