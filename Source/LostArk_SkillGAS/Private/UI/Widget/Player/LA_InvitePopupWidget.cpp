ba0ebb1d8ed4a75ca69d5127b43952e97f9093c9
gimunkwon <gimon71@gmail.com>
2026-02-02 17:27:28 +0900

    Update: Add to PartyAccept Button Widget

---
+++ Source/LostArk_SkillGAS/Private/UI/Widget/Player/LA_InvitePopupWidget.cpp
@@ -0,0 +1,27 @@
+﻿#include "UI/Widget/Player/LA_InvitePopupWidget.h"
+
+#include "Controller/Player/LA_PlayerController.h"
+
+void ULA_InvitePopupWidget::OnAcceptClicked()
+{
+	UE_LOG(LogTemp,Warning,TEXT("Party Invite Accepted"));
+	
+	// 컨트롤러를 통해 서버에 수락 신호 전달
+	if (ALA_PlayerController* PC = Cast<ALA_PlayerController>(GetOwningPlayer()))
+	{
+		PC->Server_ReplyToInvite(InviterCharacter, true);
+	}
+	RemoveFromParent();
+}
+
+void ULA_InvitePopupWidget::OnDeclineClicked()
+{
+	UE_LOG(LogTemp,Warning,TEXT("Party Invite Declined"));
+	
+	// 컨트롤러를 통해 서버에 거절 신호 전달
+	if (ALA_PlayerController* PC = Cast<ALA_PlayerController>(GetOwningPlayer()))
+	{
+		PC->Server_ReplyToInvite(InviterCharacter, false);
+	}
+	RemoveFromParent();
+}
---
+++ Source/LostArk_SkillGAS/Public/UI/Widget/Player/LA_InvitePopupWidget.h
@@ -0,0 +1,26 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LA_InvitePopupWidget.generated.h"
+
+class ALA_BaseCharacter;
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ULA_InvitePopupWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	// 나를 초대한 사람 정보(팝업 생성 시 전달 받음)
+	UPROPERTY(BlueprintReadOnly, Category="Party")
+	TObjectPtr<ALA_BaseCharacter> InviterCharacter;
+	
+	// 수락 버튼 클릭 시
+	UFUNCTION(BlueprintCallable, Category="Party")
+	void OnAcceptClicked();
+	
+	// 거절 버튼 클릭시
+	UFUNCTION(BlueprintCallable, Category="Party")
+	void OnDeclineClicked();
+};
