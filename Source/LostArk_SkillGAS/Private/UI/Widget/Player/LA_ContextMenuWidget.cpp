2a13de65e8f18908f55e11b0434366131d42a558
gimunkwon <gimon71@gmail.com>
2026-02-02 17:29:18 +0900

    Update: 파티 초대 위젯방식을 컨트롤러 에게 직접 요청하는 방식이 아닌 HUD에게 델리게이트를 전송해서 HUD쪽에서 실행을 하도록 수정

---
+++ Source/LostArk_SkillGAS/Private/UI/Widget/Player/LA_ContextMenuWidget.cpp
@@ -0,0 +1,14 @@
+﻿#include "UI/Widget/Player/LA_ContextMenuWidget.h"
+
+#include "Controller/Player/LA_PlayerController.h"
+#include "UI/HUD/LA_HUD.h"
+
+void ULA_ContextMenuWidget::OnInviteButtonClicked()
+{
+	// 클릭을 당한 플레이어에게 InvitePopupWidget을 띄워라
+	if (TargetCharacter && OnInviteRequested.IsBound())
+	{
+		OnInviteRequested.Broadcast(TargetCharacter);
+	}
+	RemoveFromParent();
+}
---
+++ Source/LostArk_SkillGAS/Public/UI/Widget/Player/LA_ContextMenuWidget.h
@@ -0,0 +1,34 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Character/Base/LA_BaseCharacter.h"
+#include "LA_ContextMenuWidget.generated.h"
+
+// 델리게이트 선언 (어떤 캐릭터를 초대할지 Target 정보를 담음)
+DECLARE_DYNAMIC_MULTICAST_DELEGATE_OneParam(FOnInviteButtonClicked, ALA_BaseCharacter*, Target);
+
+
+class ALA_BaseCharacter;
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ULA_ContextMenuWidget : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	UPROPERTY(BlueprintAssignable, Category="Events")
+	FOnInviteButtonClicked OnInviteRequested; // 델리게이트 인스턴스
+	
+	// Getter,Setter 함수
+	void SetTargetCharacter(ALA_BaseCharacter* Target) {TargetCharacter = Cast<ALA_BaseCharacter>(Target);};
+	ALA_BaseCharacter* GetTargetCharacter() const{return TargetCharacter;};
+	
+	// 블루 프린트의 버튼 클릭 이벤트와 연결될 함수
+	UFUNCTION(BlueprintCallable, Category="Party")
+	void OnInviteButtonClicked();
+protected:
+	// 컨텍스트 메뉴가 들때 어떤플레이어를 찍었는지 저장
+	UPROPERTY(BlueprintReadOnly, Category="Party", meta=(ExposeOnSpawn = true))
+	TObjectPtr<ALA_BaseCharacter> TargetCharacter;
+};
