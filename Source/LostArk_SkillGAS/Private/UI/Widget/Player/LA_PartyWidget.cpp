c14c1368d096f3ef9b530df5737f317f2b98d5a1
gimunkwon <gimon71@gmail.com>
2026-02-03 23:10:51 +0900

    Update: Add To PartyWidget이 SlotWidget을 쌓을수 있도록 로직 추가

--- Source/LostArk_SkillGAS/Private/UI/Widget/Player/LA_PartyWidget.cpp
+++ Source/LostArk_SkillGAS/Private/UI/Widget/Player/LA_PartyWidget.cpp
@@ -1,1 +1,43 @@
 ﻿#include "UI/Widget/Player/LA_PartyWidget.h"
+
+#include "Components/VerticalBox.h"
+#include "GameFramework/GameStateBase.h"
+#include "PlayerState/LA_PlayerState.h"
+#include "UI/Widget/Player/LA_PartyMemberSlot.h"
+
+void ULA_PartyWidget::RefreshPartyList()
+{
+	if (!PartyMemberBox || !SlotClass) return;
+	
+	// 기존에 생성되어 있던 슬롯들 제거
+	PartyMemberBox->ClearChildren();
+	
+	// 2. 내 PartyID 확인
+	ALA_PlayerState* MyPS = Cast<ALA_PlayerState>(GetOwningPlayerState());
+	if (!MyPS || !MyPS->GetPartyID().IsValid()) return;
+	
+	FGuid MyPartyID = MyPS->GetPartyID();
+	
+	// 3. 전체 플레이어를 순회하며 파티원 찾기
+	if (AGameStateBase* GS = GetWorld()->GetGameState())
+	{
+		for (APlayerState* PS : GS->PlayerArray)
+		{
+			ALA_PlayerState* TargetPS = Cast<ALA_PlayerState>(PS);
+			if (TargetPS && TargetPS->GetPartyID() == MyPartyID)
+			{
+				// 4. 슬롯 생성 및 데이터 주입
+				ULA_PartyMemberSlot* NewSlot = CreateWidget<ULA_PartyMemberSlot>(GetWorld(),SlotClass);
+				if (NewSlot)
+				{
+					NewSlot->SetupSlot(TargetPS);
+					PartyMemberBox->AddChildToVerticalBox(NewSlot);
+				}
+			}
+		}
+	}
+	GetWorld()->GetTimerManager().SetTimerForNextTick([this]()
+	{
+		this->RefreshPartyList();
+	});
+}
--- Source/LostArk_SkillGAS/Public/UI/Widget/Player/LA_PartyWidget.h
+++ Source/LostArk_SkillGAS/Public/UI/Widget/Player/LA_PartyWidget.h
@@ -4,8 +4,23 @@
 #include "Blueprint/UserWidget.h"
 #include "LA_PartyWidget.generated.h"
 
+class ULA_PartyMemberSlot;
+class UVerticalBox;
+
 UCLASS()
 class LOSTARK_SKILLGAS_API ULA_PartyWidget : public UUserWidget
 {
 	GENERATED_BODY()
+public:
+	// 파티 리스트를 갱신하는 함수
+	void RefreshPartyList();
+	
+protected:
+	// 슬롯들이 쌓일 공간
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UVerticalBox> PartyMemberBox;
+	
+	// 생성할 슬롯 위젯 클래스
+	UPROPERTY(EditAnywhere, Category="UI")
+	TSubclassOf<ULA_PartyMemberSlot> SlotClass;
 };
