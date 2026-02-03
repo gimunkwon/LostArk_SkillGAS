0e0b7ae1957c5d9e94782112b6058199f96b451d
gimunkwon <gimon71@gmail.com>
2026-02-03 15:48:10 +0900

    Update: Test Party Widget

---
+++ Source/LostArk_SkillGAS/Private/UI/Widget/Player/LA_PartyWidget.cpp
@@ -0,0 +1,1 @@
+﻿#include "UI/Widget/Player/LA_PartyWidget.h"
---
+++ Source/LostArk_SkillGAS/Public/UI/Widget/Player/LA_PartyWidget.h
@@ -0,0 +1,11 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "LA_PartyWidget.generated.h"
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ULA_PartyWidget : public UUserWidget
+{
+	GENERATED_BODY()
+};
