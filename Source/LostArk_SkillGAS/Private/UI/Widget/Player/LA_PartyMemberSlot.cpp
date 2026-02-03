5b2b90d8527bb5b0af149495ad2fce9128b62b8e
gimunkwon <gimon71@gmail.com>
2026-02-03 23:10:23 +0900

    Update: Add To Party Member Stat SlotWidget

---
+++ Source/LostArk_SkillGAS/Private/UI/Widget/Player/LA_PartyMemberSlot.cpp
@@ -0,0 +1,35 @@
+﻿#include "UI/Widget/Player/LA_PartyMemberSlot.h"
+
+#include "Components/ProgressBar.h"
+#include "Components/TextBlock.h"
+#include "PlayerState/LA_PlayerState.h"
+
+void ULA_PartyMemberSlot::SetupSlot(ALA_PlayerState* InPs)
+{
+	if (!InPs) return;
+	TargetPS = InPs;
+	
+	// 1. 초기 텍스트 설정
+	PlayerNameText->SetText(FText::FromString(InPs->GetPlayerName()));
+	
+	// 2. GAS Attribute 변경 감지 바인딩
+	if (UAbilitySystemComponent* ASC = InPs->GetAbilitySystemComponent())
+	{
+		// 체력 속성이 변할 때마다 OnHealthChanged를 실행하도록 바인딩
+		ASC->GetGameplayAttributeValueChangeDelegate(ULA_BaseAttributeSet::GetHealthAttribute()).AddUObject(this, &ULA_PartyMemberSlot::OnHealthChanged);
+		
+		// 3. 현재 값으로 초기 HP 설정
+		float CurrentHP = InPs->GetBaseAttributeSet()->GetHealth();
+		float MaxHP = InPs->GetBaseAttributeSet()->GetMaxHealth();
+		HealthBar->SetPercent(MaxHP > 0.f ? CurrentHP / MaxHP : 0.f);
+	}
+}
+
+void ULA_PartyMemberSlot::OnHealthChanged(const FOnAttributeChangeData& Data)
+{
+	if (TargetPS.IsValid())
+	{
+		float MaxHP = TargetPS->GetBaseAttributeSet()->GetMaxHealth();
+		HealthBar->SetPercent(MaxHP > 0.f ? Data.NewValue / MaxHP : 0.f);
+	}
+}
---
+++ Source/LostArk_SkillGAS/Public/UI/Widget/Player/LA_PartyMemberSlot.h
@@ -0,0 +1,33 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "Blueprint/UserWidget.h"
+#include "Character/Base/AttributeSet/LA_BaseAttributeSet.h"
+#include "LA_PartyMemberSlot.generated.h"
+
+class UTextBlock;
+class UProgressBar;
+class ALA_PlayerState;
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ULA_PartyMemberSlot : public UUserWidget
+{
+	GENERATED_BODY()
+	
+public:
+	// 데이터를 주입하고 초기화하는 핵심 함수
+	void SetupSlot(ALA_PlayerState* InPs);
+	
+protected:
+	// HP 변경 시 호출될 콜백
+	void OnHealthChanged(const FOnAttributeChangeData& Data);
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UProgressBar> HealthBar;
+	
+	UPROPERTY(meta=(BindWidget))
+	TObjectPtr<UTextBlock> PlayerNameText;
+	
+	// 관찰 대상 (약참조로 안전하게 관리)
+	TWeakObjectPtr<ALA_PlayerState> TargetPS;
+};
