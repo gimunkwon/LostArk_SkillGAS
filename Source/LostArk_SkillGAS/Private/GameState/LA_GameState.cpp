2380fa83346b3ee730558296041ece6b3d4842d0
gimunkwon <gimon71@gmail.com>
2026-02-03 15:47:29 +0900

    Update: Add To Party System in GameState

---
+++ Source/LostArk_SkillGAS/Private/GameState/LA_GameState.cpp
@@ -0,0 +1,31 @@
+﻿#include "GameState/LA_GameState.h"
+
+#include "PlayerState/LA_PlayerState.h"
+
+void ALA_GameState::RegisterPartyMember(FGuid PartyID, ALA_PlayerState* NewMember, bool bIsLeader)
+{
+	if (!NewMember) return;
+	
+	// 기존 파티가 있는지 확인, 없으면 새로 생성
+	FLA_PartyInfo& PartyInfo = PartyMap.FindOrAdd(PartyID);
+	
+	// 새로 생성된 경우
+	if (PartyInfo.PartyID == FGuid())
+	{
+		PartyInfo.Members.Add(NewMember);
+	}
+	
+	// 3. 파티장 설정
+	if (bIsLeader)
+	{
+		PartyInfo.Leader = NewMember;
+	}
+	
+	UE_LOG(LogTemp,Warning,TEXT("GameState: Player %s Registered to Party %s")
+		,*NewMember->GetPlayerName(), *PartyID.ToString());
+}
+
+FLA_PartyInfo* ALA_GameState::GetPartyInfo(FGuid PartyID)
+{
+	return PartyMap.Find(PartyID);
+}
---
+++ Source/LostArk_SkillGAS/Public/GameState/LA_GameState.h
@@ -0,0 +1,22 @@
+﻿#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameStateBase.h"
+#include "Global/Party/LA_PartyManager.h"
+#include "LA_GameState.generated.h"
+
+
+UCLASS()
+class LOSTARK_SKILLGAS_API ALA_GameState : public AGameStateBase
+{
+	GENERATED_BODY()
+	
+public:
+	// 서버에서 관리하는 전체 파티 맵
+	TMap<FGuid, FLA_PartyInfo> PartyMap;
+	
+	// 파티 생성 또는 멤버 추가 함수
+	void RegisterPartyMember(FGuid PartyID, ALA_PlayerState* NewMember, bool bIsLeader);
+	// 파티 정보 조회
+	FLA_PartyInfo* GetPartyInfo(FGuid PartyID);
+};
