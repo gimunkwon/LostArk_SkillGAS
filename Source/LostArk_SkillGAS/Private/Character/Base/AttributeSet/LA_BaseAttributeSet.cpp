88f8168a4d3a32d57d8d6b7918566da61410e6fe
gimunkwon <gimon71@gmail.com>
2026-02-03 23:10:00 +0900

    Update: OnRep Function && Variable ReplicatedUsing Add

--- Source/LostArk_SkillGAS/Private/Character/Base/AttributeSet/LA_BaseAttributeSet.cpp
+++ Source/LostArk_SkillGAS/Private/Character/Base/AttributeSet/LA_BaseAttributeSet.cpp
@@ -1,4 +1,32 @@
-﻿
+﻿#include "Character/Base/AttributeSet/LA_BaseAttributeSet.h"
+
+#include "Net/UnrealNetwork.h"
+
+// 네트워크 복제 규칙 정의
+void ULA_BaseAttributeSet::GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const
+{
+	Super::GetLifetimeReplicatedProps(OutLifetimeProps);
+	
+	// 속성들이 네트워크를 통해 복제되도록 등록
+	DOREPLIFETIME_CONDITION_NOTIFY(ULA_BaseAttributeSet, Health, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(ULA_BaseAttributeSet, MaxHealth, COND_None, REPNOTIFY_Always);
+	DOREPLIFETIME_CONDITION_NOTIFY(ULA_BaseAttributeSet, AttackPower, COND_None, REPNOTIFY_Always);
+}
+
+void ULA_BaseAttributeSet::OnRep_Health(const FGameplayAttributeData& OldHealth)
+{
+	// 이 매크로가 클라이언트의 ASC 델리게이트를 트리거
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ULA_BaseAttributeSet, Health, OldHealth);
+}
+
+void ULA_BaseAttributeSet::OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ULA_BaseAttributeSet, MaxHealth, OldMaxHealth);
+}
+
+void ULA_BaseAttributeSet::OnRep_AttackPower(const FGameplayAttributeData& OldAttackPower)
+{
+	GAMEPLAYATTRIBUTE_REPNOTIFY(ULA_BaseAttributeSet, AttackPower, OldAttackPower);
+}
 
 
-#include "Character/Base/AttributeSet/LA_BaseAttributeSet.h"
--- Source/LostArk_SkillGAS/Public/Character/Base/AttributeSet/LA_BaseAttributeSet.h
+++ Source/LostArk_SkillGAS/Public/Character/Base/AttributeSet/LA_BaseAttributeSet.h
@@ -12,15 +12,28 @@ class LOSTARK_SKILLGAS_API ULA_BaseAttributeSet : public UAttributeSet
 	GENERATED_BODY()
 public:
 	// HP
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_Health ,Category="Attributes")
 	FGameplayAttributeData Health;
 	ATTRIBUTE_ACCESSORS(ULA_BaseAttributeSet, Health);
 	// MAXHP
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_MaxHealth ,Category="Attributes")
 	FGameplayAttributeData MaxHealth;
 	ATTRIBUTE_ACCESSORS(ULA_BaseAttributeSet, MaxHealth);
 	// 공격력
-	UPROPERTY(BlueprintReadOnly, Category="Attributes")
+	UPROPERTY(BlueprintReadOnly,ReplicatedUsing = OnRep_AttackPower,Category="Attributes")
 	FGameplayAttributeData AttackPower;
 	ATTRIBUTE_ACCESSORS(ULA_BaseAttributeSet, AttackPower);
+	
+	// OnRep 함수 선언 (클라이언트 알림용)
+	UFUNCTION()
+	virtual void OnRep_Health(const FGameplayAttributeData& OldHealth);
+	
+	UFUNCTION()
+	virtual void OnRep_MaxHealth(const FGameplayAttributeData& OldMaxHealth);
+	
+	UFUNCTION()
+	virtual void OnRep_AttackPower(const FGameplayAttributeData& OldAttackPower);
+	
+	// 네트워크 복제 등록
+	virtual void GetLifetimeReplicatedProps(TArray<class FLifetimeProperty>& OutLifetimeProps) const override;
 };
