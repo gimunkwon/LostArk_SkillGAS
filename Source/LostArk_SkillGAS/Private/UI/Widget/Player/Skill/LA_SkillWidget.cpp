aa45c0d4757c9f31a00b15aec155611f5662cd55
gimunkwon <gimon71@gmail.com>
2026-02-04 20:23:18 +0900

    Update: Add to DragUpDown Widget Logic

--- Source/LostArk_SkillGAS/Private/UI/Widget/Player/Skill/LA_SkillWidget.cpp
+++ Source/LostArk_SkillGAS/Private/UI/Widget/Player/Skill/LA_SkillWidget.cpp
@@ -1,4 +1,69 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
+﻿#include "UI/Widget/Player/Skill/LA_SkillWidget.h"
+
+#include "Blueprint/WidgetLayoutLibrary.h"
+#include "Components/CanvasPanelSlot.h"
+#include "Blueprint/SlateBlueprintLibrary.h"
 
 
-#include "UI/Widget/Player/Skill/LA_SkillWidget.h"
+FReply ULA_SkillWidget::NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseButtonDown(InGeometry, InMouseEvent);
+	// 왼쪽 마우스 버튼인지 확인
+	if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+	{
+		bIsDragging = true;
+		
+		// 현재 마우스의 절대 위치를 뷰포트 위치로 변환
+		FVector2D MouseViewportPos;
+		FVector2D Dummy;
+		USlateBlueprintLibrary::AbsoluteToViewport(GetWorld(), InMouseEvent.GetScreenSpacePosition(), MouseViewportPos, Dummy);
+		
+		// 현재 위젯의 절대 위치를 뷰포트 위치로 변환
+		FVector2D WidgetViewportPos;
+		USlateBlueprintLibrary::AbsoluteToViewport(GetWorld(), InGeometry.GetAbsolutePosition(), WidgetViewportPos, Dummy);
+		
+		// 뷰포트 기준의 오프셋 계산
+		DragOffset = MouseViewportPos - WidgetViewportPos;
+		
+		// 마우스 포커스를 이 위젯으로 고정
+		return FReply::Handled().CaptureMouse(TakeWidget());
+	}
+	
+	return FReply::Unhandled();
+}
+
+FReply ULA_SkillWidget::NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseMove(InGeometry, InMouseEvent);
+	
+	if (bIsDragging)
+	{
+		// 현재 마우스의 절대 위치를 뷰포트 위치로 변환
+		FVector2D MouseViewportPos;
+		FVector2D Dummy;
+		USlateBlueprintLibrary::AbsoluteToViewport(GetWorld(), InMouseEvent.GetScreenSpacePosition(), MouseViewportPos, Dummy);
+		// 오프셋을 적용한 새로운 뷰포트 위치 계산
+		FVector2D NewPosition = MouseViewportPos - DragOffset;
+		
+		
+		// AddToViewport로 띄운 위젯은 이 함수를 사용
+		SetPositionInViewport(NewPosition);
+		
+		return FReply::Handled();
+	}
+	return FReply::Unhandled();
+}
+
+FReply ULA_SkillWidget::NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent)
+{
+	Super::NativeOnMouseButtonUp(InGeometry, InMouseEvent);
+	
+	if (InMouseEvent.GetEffectingButton() == EKeys::LeftMouseButton)
+	{
+		bIsDragging = false;
+		// 마우스 캡처 해제
+		return FReply::Handled().ReleaseMouseCapture();
+	}
+	
+	return FReply::Unhandled(); 
+}
--- Source/LostArk_SkillGAS/Public/UI/Widget/Player/Skill/LA_SkillWidget.h
+++ Source/LostArk_SkillGAS/Public/UI/Widget/Player/Skill/LA_SkillWidget.h
@@ -1,16 +1,24 @@
-﻿// Fill out your copyright notice in the Description page of Project Settings.
-
-#pragma once
+﻿#pragma once
 
 #include "CoreMinimal.h"
 #include "Blueprint/UserWidget.h"
 #include "LA_SkillWidget.generated.h"
 
-/**
- * 
- */
 UCLASS()
 class LOSTARK_SKILLGAS_API ULA_SkillWidget : public UUserWidget
 {
 	GENERATED_BODY()
+	
+protected:
+	// 마우스 버튼을 눌렀을 때 실행
+	virtual FReply NativeOnMouseButtonDown(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	// 마우스가 움직일 때 실행
+	virtual FReply NativeOnMouseMove(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+	// 마우스 버튼을 뗏을 때 실행
+	virtual FReply NativeOnMouseButtonUp(const FGeometry& InGeometry, const FPointerEvent& InMouseEvent) override;
+private:
+	// 마우스 버튼을 눌렀는지?
+	bool bIsDragging = false;
+	// 마우스 클릭 지점과 위젯 좌상단 사이의 거리 (오프셋)
+	FVector2D DragOffset;
 };
